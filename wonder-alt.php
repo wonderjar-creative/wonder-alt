2a91f8126042911b9a6eacf024b918e7dcf63b3b
Matthew Ediger <matt@mediaworksweb.com>
2025-04-01 16:30:40 -0600

    chore: minor version bump

--- wonder-alt.php
+++ wonder-alt.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Wonder Alt
  * Plugin URI:        https://wonderjarcreative.com
  * Description:       Automatically add alt text to images.
- * Version:           1.4.1
+ * Version:           1.4.2
  * Author:            Matthew Ediger
  * Author URI:        https://wonderjarcreative.com/
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if (! defined('WPINC')) {
  * Start at version 0.1.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('WONDER_ALT_VERSION', '1.4.1');
+define('WONDER_ALT_VERSION', '1.4.2');
 
 /**
  * The code that runs during plugin activation.
